0241eb95c1ac91e52914f504009e36769fcd9c6d
mychidarko <mickdd22@gmail.com>
2022-04-08 23:33:08 +0000

    :sparkles: update dotfiles

---
+++ setup.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+# install homebrew
+if test ! $(which brew); then
+    echo "Installing Homebrew..."
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+else
+    echo "Updating Homebrew..."
+    brew update
+fi
+
+# Register aliases, functions, ohmyzsh stuff, ...
+if [ -f "$HOME/.zshrc" ]; then
+    echo "Configuring zsh..."
+    echo 'source ~/.bash/.bash_aliases' >> ~/.zshrc
+    echo 'source ~/.bash/bash_completions.sh' >> ~/.zshrc
+    echo 'source ~/.bash/bash_functions.sh' >> ~/.zshrc
+else 
+    echo "Configuring bash..."
+    echo 'source ~/.bash/.bash_aliases' >> ~/.bashrc
+    echo 'source ~/.bash/bash_completions.sh' >> ~/.bashrc
+    echo 'source ~/.bash/bash_functions.sh' >> ~/.bashrc
+fi
+
+echo "Configuration complted successfully!"
+echo "Configuring git user..."
+
+# set default git config
+git config --global user.name "mychidarko"
+git config --global user.email "mickdd22@gmail.com"
+git config --global color.ui true
+
+echo "Git configured successfully!"
+echo "Installing important packages..."
+
+# Install most used important packages
+installables=(
+    git
+    wget
+    ruby
+    python
+    node
+    php
+)
+
+brew install ${installables[@]}
+
+# Install yarn
+npm install -g yarn
+
+# setup folders
+mkdir ~/projects
